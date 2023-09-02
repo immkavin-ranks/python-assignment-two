0dd4e0caf39b900dc874d0972512411dd45545cd
Kavin <126098744+immkavin-ranks@users.noreply.github.com>
2023-09-02 10:59:29 +0530

    Update palindrome.py

--- palindrome.py
+++ palindrome.py
@@ -3,25 +3,15 @@ def is_palindrome(input_str):
     return input_str == input_str[::-1]
 
 while True:
-    choice = input("Enter '1' to check a number, '2' for a string, or '3' to quit: ")
+    choice = input("Enter '1' to check, '2' to quit: ")
     
-    if choice == '3':
+    if choice == '2':
         print("Goodbye!")
         break
     
     input_str = input("Enter a number or a string: ")
     
-    if choice == '1':
-        if input_str.isdigit() and is_palindrome(input_str):
-            print(f"{input_str} is a palindrome!")
-        else:
-            print(f"{input_str} is not a palindrome.")
-    
-    elif choice == '2':
-        if is_palindrome(input_str):
-            print(f'"{input_str}" is a palindrome!')
-        else:
-            print(f'"{input_str}" is not a palindrome.')
-    
+    if is_palindrome(input_str):
+        print(f'"{input_str}" is a palindrome!')
     else:
-        print("Invalid choice. Please choose 1, 2, or 3.")
+        print(f'"{input_str}" is not a palindrome.')
